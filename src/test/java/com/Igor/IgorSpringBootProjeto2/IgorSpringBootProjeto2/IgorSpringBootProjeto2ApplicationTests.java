07ed80cc2daf6724fcb72514da36125144ab23c5
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-01-19 12:36:58 -0300

    Add files via upload

---
+++ src/test/java/com/Igor/IgorSpringBootProjeto2/IgorSpringBootProjeto2/IgorSpringBootProjeto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Igor.IgorSpringBootProjeto2.IgorSpringBootProjeto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgorSpringBootProjeto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
