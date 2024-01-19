0471585f08a7bcfa942b4987f00135277be35c58
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-01-19 12:30:14 -0300

    Add files via upload

---
+++ src/main/java/com/Igor/IgorSpringBootProjeto2/IgorSpringBootProjeto2/IgorSpringBootProjeto2Application.java
@@ -0,0 +1,13 @@
+package com.Igor.IgorSpringBootProjeto2.IgorSpringBootProjeto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorSpringBootProjeto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgorSpringBootProjeto2Application.class, args);
+	}
+
+}
