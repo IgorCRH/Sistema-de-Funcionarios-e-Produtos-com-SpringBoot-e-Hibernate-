a2c255fcd34309361a4eb19576a3bcc46164bc78
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-01-19 12:29:46 -0300

    Add files via upload

---
+++ src/main/java/com/Igor/IgorSpringBootProjeto2/IgorSpringBootProjeto2/config/JpaConfig.java
@@ -0,0 +1,25 @@
+package com.Igor.IgorSpringBootProjeto2.IgorSpringBootProjeto2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.Igor.IgorSpringBootProjeto2.IgorSpringBootProjeto2.model");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        return em;
+    }
+}
